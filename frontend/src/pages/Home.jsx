709a376746c67096fa744c0aedb9388bd84605de
Attila Simon <simon.attila.fr@gmail.com>
2022-07-22 11:46:52 +0200

    Home content and structure updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useContext } from "react";
 import axios from "axios";
 import MainContext from "../contexts/MainContext";
+import HomeTitle from "../components/HomeTitle";
+import HomeSubtitle from "../components/HomeSubtitle";
+import landingPageImage from "../assets/homeImage1.png";
+import "./Home.css";
 
 export default function Home() {
   const { initialContent, setInitialContent } = useContext(MainContext);
@@ -13,6 +17,18 @@ export default function Home() {
 
   return (
     <div className="homeContainer">
+      <section className="titleArea">
+        <div className="imageArea">
+          <img
+            src={landingPageImage}
+            alt="La tête d'une femme aux trois bouches."
+          />
+        </div>
+        <div className="textArea">
+          <HomeTitle />
+          <HomeSubtitle />
+        </div>
+      </section>
       {initialContent
         .filter((e) => e.ref.includes("home"))
         .map((e) => (
