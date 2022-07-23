4a1d5912db0ac56d43892ddc97fbe6d6ddf7a052
Attila Simon <simon.attila.fr@gmail.com>
2022-07-23 15:51:41 +0200

    Page skeleton log in & sign up added

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,3 +1,17 @@
+import "./Form.css";
+
 export default function Signup() {
-  return <h1>Sign up</h1>;
+  return (
+    <div className="formContainer">
+      <h1>Welcome!</h1>
+      <form type="submit" className="form">
+        <label htmlFor="pseudo">Username:</label>
+        <input type="text" name="pseudo" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" minLength="8" />
+        <label htmlFor="passwordsecu">Repeat the password:</label>
+        <input type="password" name="passwordsecu" minLength="8" />
+      </form>
+    </div>
+  );
 }
